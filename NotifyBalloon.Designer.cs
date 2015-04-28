75460c4ed950ef37243b6d017ea86dc58ecce458
Tolga Evcimen <h.tolgaevcimen@gmail.com>
2015-04-28 17:29:51 +0300

    Whole Project is added to the repository

---
+++ NotifyBalloon.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace NotifyBalloon
+{
+    partial class NotifyBalloon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCloseBalloon = new System.Windows.Forms.Button();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.89918F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.10082F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.Controls.Add(this.btnCloseBalloon, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblCaption, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblText, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(290, 48);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnCloseBalloon
+            // 
+            this.btnCloseBalloon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCloseBalloon.FlatAppearance.BorderSize = 0;
+            this.btnCloseBalloon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseBalloon.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCloseBalloon.Location = new System.Drawing.Point(256, 0);
+            this.btnCloseBalloon.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCloseBalloon.Name = "btnCloseBalloon";
+            this.btnCloseBalloon.Size = new System.Drawing.Size(34, 25);
+            this.btnCloseBalloon.TabIndex = 0;
+            this.btnCloseBalloon.Text = "x";
+            this.btnCloseBalloon.UseVisualStyleBackColor = true;
+            this.btnCloseBalloon.Click += new System.EventHandler(this.closeButton_Click);
+            this.btnCloseBalloon.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            this.btnCloseBalloon.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaption.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCaption.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblCaption.Location = new System.Drawing.Point(32, 0);
+            this.lblCaption.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(220, 25);
+            this.lblCaption.TabIndex = 1;
+            this.lblCaption.Text = "label1";
+            this.lblCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblCaption.Click += new System.EventHandler(this.NotifyBalloon_Clicked);
+            this.lblCaption.MouseEnter += new System.EventHandler(this.NotifyBalloon_MouseEnter);
+            this.lblCaption.MouseLeave += new System.EventHandler(this.NotifyBalloon_MouseLeave);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.lblText, 3);
+            this.lblText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblText.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblText.Location = new System.Drawing.Point(2, 27);
+            this.lblText.Margin = new System.Windows.Forms.Padding(2);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(286, 19);
+            this.lblText.TabIndex = 2;
+            this.lblText.Text = "label2";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblText.Click += new System.EventHandler(this.NotifyBalloon_Clicked);
+            this.lblText.MouseEnter += new System.EventHandler(this.NotifyBalloon_MouseEnter);
+            this.lblText.MouseLeave += new System.EventHandler(this.NotifyBalloon_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Image = global::NotifyBalloon.Properties.Resources.rsz_facebook_verified_account_logo;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.NotifyBalloon_Clicked);
+            this.label1.MouseEnter += new System.EventHandler(this.NotifyBalloon_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.NotifyBalloon_MouseLeave);
+            // 
+            // NotifyBalloon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(292, 50);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(292, 300);
+            this.MinimumSize = new System.Drawing.Size(292, 50);
+            this.Name = "NotifyBalloon";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.MouseEnter += new System.EventHandler(this.NotifyBalloon_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.NotifyBalloon_MouseLeave);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnCloseBalloon;
+        private System.Windows.Forms.Label lblCaption;
+        public System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ NotifyBalloon.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+
+namespace NotifyBalloon
+{
+    public partial class NotifyBalloon : Form
+    {
+        public int StallDuration { get; set; }
+
+        MouseHookListener MouseHooker = new MouseHookListener(new GlobalHooker());
+        /// <summary>
+        /// Parameterless NotifyBalloon constructor.
+        /// REMARK: Parameterless overload of the show method should not be called without setting the stallDuration, caption and text.
+        /// </summary>
+        public NotifyBalloon ()
+        {
+            InitializeComponent();
+
+            MouseHooker.MouseMove += MouseHooker_MouseMove;
+            MouseHooker.Enabled = true;
+
+            Click += NotifyBalloon_Clicked;
+            FormClosed += NotifyBalloon_FormClosed;
+        }
+
+        /// <summary>
+        /// NotifyBalloon constructor with the given parameters to preset balloon values.
+        /// REMARK: Calling both overloads of 'Show' method would work.
+        /// </summary>
+        /// <param name="stallDuration">how long should balloon display</param>
+        /// <param name="caption">title of the baloon</param>
+        /// <param name="text">message to be shown in the balloon</param>
+        public NotifyBalloon ( int stallDuration, string caption, string text )
+        {
+            InitializeComponent();
+
+            MouseHooker.MouseMove += MouseHooker_MouseMove;
+            MouseHooker.Enabled = true;
+
+            Click += NotifyBalloon_Clicked;
+            FormClosed += NotifyBalloon_FormClosed;
+
+            StallDuration = stallDuration;
+            lblCaption.Text = caption;
+            lblText.Text = text;
+
+            SetLocationAndSize();
+        }
+
+        void SetLocationAndSize ()
+        {
+            this.TopMost = true;
+            var trayRect = WinAPI.GetTrayRectangle();
+
+            var sizeOfText = TextRenderer.MeasureText(lblText.Text, lblText.Font);
+            var rate = (float)sizeOfText.Width / ( this.Width - 4 );
+
+            var rowCount = (int)Math.Ceiling(rate) + ( rate - Math.Floor(rate) > 0.2 ? 1 : 0 );
+
+            this.Height = ( 21 * rowCount ) + 25;
+            this.Location = new Point(trayRect.X - ( this.Width - trayRect.Width ) - 15, trayRect.Y - this.Height - 5);
+        }
+
+        public new void Show ()
+        {
+            base.Show();
+            FadeIn(this);
+        }
+
+        /// <summary>
+        /// shows the notifyBalloon with given parameters
+        /// </summary>
+        /// <param name="stallDuration">how long should balloon display</param>
+        /// <param name="caption">title of the baloon</param>
+        /// <param name="text">message to be shown in the balloon</param>
+        public void Show ( int stallDuration, string caption, string text )
+        {
+            StallDuration = stallDuration;
+            lblCaption.Text = caption;
+            lblText.Text = text;
+
+            SetLocationAndSize();
+
+            base.Show();
+            FadeIn(this);
+        }
+
+        #region Custom Events
+
+        public event EventHandler NotifyBalloonClicked;
+        public event EventHandler NotifyBalloonClosed;
+
+        void NotifyBalloon_Clicked ( object sender, EventArgs e )
+        {
+            EventHandler handler = this.NotifyBalloonClicked;
+            if ( handler != null )
+            {
+                handler(this, e);
+            }
+        }
+
+        bool IsClosed { get; set; }
+        void NotifyBalloon_FormClosed ( object sender, FormClosedEventArgs e )
+        {
+            if ( IsClosed ) return;
+
+            IsClosed = true;
+
+            EventHandler handler = this.NotifyBalloonClosed;
+            if ( handler != null )
+            {
+                handler(this, e);
+            }
+        }
+
+        #endregion
+
+        #region Events and Properties for closing the notificationBaloon properly
+
+        public bool MouseOver { get; set; }
+        private async void MouseHooker_MouseMove ( object sender, MouseEventArgs e )
+        {
+            MouseHooker.MouseMove -= MouseHooker_MouseMove;
+            MouseHooker.Dispose();
+
+            await Task.Delay(StallDuration);
+            if ( !MouseOver )
+                FadeOut(this);
+        }
+        void NotifyBalloon_MouseEnter ( object sender, EventArgs e )
+        {
+            MouseOver = true;
+        }
+        async void NotifyBalloon_MouseLeave ( object sender, EventArgs e )
+        {
+            MouseOver = false;
+
+            await Task.Delay(StallDuration);
+            if ( !MouseOver )
+                FadeOut(this);
+        }
+
+        private void closeButton_Click ( object sender, EventArgs e )
+        {
+            lblCaption.MouseEnter -= NotifyBalloon_MouseEnter;
+            lblCaption.MouseLeave -= NotifyBalloon_MouseLeave;
+            lblText.MouseEnter -= NotifyBalloon_MouseEnter;
+            lblText.MouseLeave -= NotifyBalloon_MouseEnter;
+            label1.MouseEnter -= NotifyBalloon_MouseEnter;
+            label1.MouseLeave -= NotifyBalloon_MouseEnter;
+            this.MouseEnter -= NotifyBalloon_MouseEnter;
+            this.MouseLeave -= NotifyBalloon_MouseEnter;
+
+            Close();
+        }
+
+        #endregion
+
+        #region Opening and Closing form by fading them in and out
+
+        private async void FadeIn ( Form o, int interval = 40 )
+        {
+            //Object is not fully invisible. Fade it in
+            while ( o.Opacity < 1.0 )
+            {
+                await Task.Delay(interval);
+                o.Opacity += 0.05;
+            }
+            o.Opacity = 1; //make fully visible       
+        }
+        private async void FadeOut ( Form o, int interval = 40 )
+        {
+            //Object is fully visible. Fade it out
+            while ( o.Opacity > 0.0 )
+            {
+                if ( MouseOver )
+                {
+                    o.Opacity = 1.0;
+                    return;
+                }
+
+                await Task.Delay(interval);
+                o.Opacity -= 0.05;
+            }
+            o.Opacity = 0; //make fully invisible    
+
+            o.Close();
+        }
+
+        #endregion
+        #region Changing CloseButton's font
+
+        private void closeButton_MouseEnter ( object sender, EventArgs e )
+        {
+            var font = ( (Button)sender ).Font;
+
+            ( (Button)sender ).Font = new Font(font, FontStyle.Bold);
+
+            font.Dispose();
+        }
+        private void closeButton_MouseLeave ( object sender, EventArgs e )
+        {
+            var font = ( (Button)sender ).Font;
+
+            ( (Button)sender ).Font = new Font(font, FontStyle.Regular);
+
+            font.Dispose();
+        }
+
+        #endregion
+    }
+}
